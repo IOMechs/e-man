6dd8cf977580ddec7c3128a2f3cfed8c16f61c00
Muhammad Mehdi <mehdi.rajani@iomechs.com>
2018-08-17 16:37:45 +0500

    worked on sidebar username and email bindings

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../core/services/user/user.service';
 
 @Component({
   selector: 'em-admin',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService: UserService) { }
+  user:any;
+  
   ngOnInit() {
+    this.getUser();
+  }
+
+  getUser(){
+    this.user = this.userService.getUser();
   }
 
 }
