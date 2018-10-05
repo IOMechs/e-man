8a94f453c4ad4289e20a4bb2213083c2abd9c952
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2018-10-05 16:27:15 +0500

    some CSS suggestions before this can be refactored properly

---
+++ client/src/app/shared/components/entity-header/entity-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'em-entity-header',
+  templateUrl: './entity-header.component.html',
+  styleUrls: ['./entity-header.component.scss']
+})
+export class EntityHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
